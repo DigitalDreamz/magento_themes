245b5d889f0fa07114fcd1ae15e55efa137302ba
artursj <artursj@ddz.fr>
2013-01-25 19:26:25 +0200

    Initial commit

---
+++ skin/frontend/bootstrapped/sexypack/js/application.js
@@ -0,0 +1,26 @@
+// carousel demo
+$.noConflict();
+jQuery(function($){
+	$('.carousel-nav a').click(function(){
+		$('#carousel1').carousel($(this).data('to'));
+	});
+
+	$('.nav-tabs a').click(function (e) {
+	  e.preventDefault();
+	  $(this).tab('show');
+	});
+	
+	$(document).keydown(function(event) {
+		var focused = $('.carousel *').is(":focus");
+		if (focused && event.keyCode === 39) { // 39 - right key
+		  $('.carousel-control.right').click();
+		}
+
+		if (focused && event.keyCode === 37) { // 37 - left key
+		  $('.carousel-control.left').click();
+		}
+	});
+	
+	$('#myModal').modal();
+});
+
